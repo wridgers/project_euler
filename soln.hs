4889e770daa447e8d16c142124423fce9e1f58ea
Will <will@mindfuzz.net>
2012-02-03 11:10:47 +0000

    Problem 3 solved\!

--- soln.hs
+++ soln.hs
@@ -1,5 +1,7 @@
 -- Project Euler
 
+import Data.List
+
 problem1 :: Integer
 problem1 = sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
@@ -13,13 +15,20 @@ fib' :: [Integer] -> [Integer]
 fib' (x:y:xs) = y:(fib' ((x+y):x:xs))
 
 problem3 :: Integer
-problem3 = 0
+problem3 = last (sort (factors 600851475143))
 
 -- list of factors
 factors :: Integer -> [Integer]
 factors n = fact' [n]
 fact' :: [Integer] -> [Integer]
-fact' (x:xs) = x:xs
+fact' (x:xs)
+        | isPrime(x)        = x:xs
+        | otherwise         = fact' ((fact'' 2 x) ++ xs)
+fact'' :: Integer -> Integer -> [Integer]
+fact'' a d
+        | not (isPrime(a))  = fact'' (a+1) d
+        | d `mod` a == 0    = (d `div` a):a:[]
+        | otherwise         = fact'' (a+1) d
 
 -- isPrime
 isPrime :: Integer -> Bool
@@ -37,3 +46,6 @@ isPrime' a r
 -- infinite list of primes
 primes :: [Integer]
 primes = [ x | x <- [1..], isPrime(x) ]
+
+problem4 :: Integer
+problem4 = 0
